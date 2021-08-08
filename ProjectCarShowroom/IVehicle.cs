b7a95a266d60c92f5ba7ca8941943b8fa853e7d8
aydogdumustafa <69818194+aydogdumustafa@users.noreply.github.com>
2021-08-08 12:52:57 +0200

    Add Vehicle Interfaces

---
+++ ProjectCarShowroom/IVehicle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectCarShowroom
+{
+    public interface IVehicle
+    {
+        string VehicleColor { get; set; }
+        string VehicleBrand { get; set; }
+        int VehiclePower { get; set; }
+        int VehicleCapasity { get; set; }
+    }
+}
